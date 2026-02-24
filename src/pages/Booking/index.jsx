32c3f38fa6e0c5c05193f5905b50df728798aa1f
joaoemanuels <jemanuel.pi@gmail.com>
2026-02-24 10:43:53 -0300

    Feat:  add new page, Booking

---
+++ src/pages/Booking/index.jsx
@@ -0,0 +1,12 @@
+import "../Booking/booking.styles.css";
+
+
+function Booking() {
+  return (
+    <>
+      <h1>teste</h1>
+    </>
+  );
+}
+
+export default Booking;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,15 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "../components/layout/Layout/index.jsx";
 import Home from "../pages/Home/index.jsx";
+import Booking from "../pages/Booking/index.jsx";
 // import Admin from "../pages/Admin";
-// import Booking from "../pages/Booking";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/booking" element={<Booking />} /> */}
+        <Route path="/booking" element={<Booking />} />
       </Route>
 
       {/* <Route path="/admin" element={<Admin />} /> */}
