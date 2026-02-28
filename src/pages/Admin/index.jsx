9434a3c05a42f7993d36ab4d2694ec99f157a1c2
joaoemanuels <jemanuel.pi@gmail.com>
2026-02-28 11:54:51 -0300

    fix: login and logout

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,9 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 import "../Admin/admin.styles.css";
 
 function Admin() {
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    localStorage.removeItem("auth");
+    navigate("/login");
+  }
+
   return (
     <>
-      <h1>teste</h1>
+      <button onClick={handleLogout}>Sair</button>
+      <h1>Painel Admin</h1>
     </>
   );
 }
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 function PrivateRoute({ children }) {
   const isAuthenticated = localStorage.getItem("auth");
 
-  return isAuthenticated ? children : <Navigate to="/login" />;
+  return isAuthenticated ? children : <Navigate to="/login" replace/>;
 }
 
 export default PrivateRoute;
